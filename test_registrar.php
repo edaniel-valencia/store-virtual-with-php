64d15c5e12b9e6c336816517c6f09acd35efb202
Edaniel Valencia <144701255+edaniel-valencia@users.noreply.github.com>
2026-08-25 12:01:34 -0500

    no message

--- test_registrar.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_SERVER['REQUEST_METHOD'] = 'POST';
-$json = json_encode(['idCliente' => '', 'pago' => 15]);
-$stream = fopen('php://memory', 'r+');
-fwrite($stream, $json);
-rewind($stream);
-// Mock php://input? Can't easily do that.
--- test_sale.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-require_once 'Librerias/class.Cart.php';
-$cart = new Cart();
-$cart->add(1, 1, ['price' => 15, 'nombre' => 'ACEITE OLIVA']);
-echo "Total: " . $cart->getAttributeTotal('price') . "\n";
-$items = $cart->getItems();
-var_dump($items);
